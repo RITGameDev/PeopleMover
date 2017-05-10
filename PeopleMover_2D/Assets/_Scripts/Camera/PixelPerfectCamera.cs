de4e2d706dc4d42a977de0de290557e84b31a439
Ben Hoffman <bah8892@g.rit.edu>
2017-05-09 21:14:04 -0400

    changed some cam settings

---
+++ PeopleMover_2D/Assets/_Scripts/Camera/PixelPerfectCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelPerfectCamera : MonoBehaviour {
+
+    public static float pixelsToUnits = 1f;
+    public static float scale = 1f;
+
+    public Vector2 nativeResoliation = new Vector2(240, 160);
+
+    private void Awake()
+    {
+        Camera cam = GetComponent<Camera>();
+        scale = Screen.height / nativeResoliation.y;
+        pixelsToUnits *= scale;
+
+        cam.orthographicSize = (Screen.height / 2.0f) / pixelsToUnits;
+    }
+}
